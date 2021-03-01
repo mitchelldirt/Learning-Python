3e7183d71b1f41a79d5be9742eea914c892a8774
Mitchell Mudd <75150470+mitchelldirt@users.noreply.github.com>
2021-03-01 12:46:13 -0500

    Add files via upload
    Completed on my lunch break.

---
+++ coding_exercises/second_index_challenge.py
@@ -0,0 +1,16 @@
+def second_index(text: str, symbol: str) -> [int, None]:
+    """
+        returns the second index of a symbol in a given text
+    """
+    i = 0
+    for thing in text:
+        if thing == symbol:
+            i += 1
+    if i < 2:
+        return None
+    else:
+        first_spot = text.index(symbol)
+        return text.index(symbol, first_spot + 1)
+
+
+print(second_index("hi", " "))
