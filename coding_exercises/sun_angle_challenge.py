872089f4259caa587943f91f99d605e4750655b2
Mitchell Mudd <75150470+mitchelldirt@users.noreply.github.com>
2021-03-13 07:53:44 -0500

    Update sun_angle_challenge.py

--- coding_exercises/sun_angle_challenge.py
+++ coding_exercises/sun_angle_challenge.py
@@ -3,11 +3,9 @@ def sun_angle(time: str):
     times = time.split(':')
     hours = int(times[0])
     minutes = int(times[1])
-    if hours < 6 or hours >= 18 and minutes > 0:
+    if (hours < 6) or (hours >= 18 and minutes > 0):
         return "I don't see the sun!"
-    if hours == 6 and minutes == 0:
-        return 0
-    if hours >= 6:
+    else: 
         hours_degrees = (hours - 6) * 15
         minutes_degrees = minutes * .25
         return hours_degrees + minutes_degrees
